b88505dfeac094fea30708ead9cddcdcff9852d6
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-02-18 13:12:53 +0700

    add image for make parallex and add lib Kinesis,Atropos and vue-parallex

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { createPinia } from 'pinia'
 import 'flowbite';
 import 'flowbite/dist/flowbite.css'
 import  'flowbite/dist/flowbite'
+import VueKinesis from "vue-kinesis";
+import 'atropos/css'
 
 import App from './App.vue'
 import router from './router'
@@ -14,6 +16,7 @@ import './assets/main.css'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(VueKinesis)
 app.use(router)
 
 app.mount('#app')
