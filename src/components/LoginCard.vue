03ee69473b0dfdf1016175c1b228c7e9eaaf3a84
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-02-19 01:14:17 +0700

    add or line in logincard and bordersocial login

--- src/components/LoginCard.vue
+++ src/components/LoginCard.vue
@@ -26,7 +26,7 @@
           <!--login google facebook and github -->
           <div class="mt-16 grid space-y-4">
             <button
-              class="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100"
+              class="group h-12 px-6 border-2 border-gray-300 dark:border-gray-700 rounded-full transition duration-300 hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100"
             >
               <div class="relative flex items-center space-x-4 justify-center">
                 <img
@@ -41,7 +41,7 @@
               </div>
             </button>
             <button
-              class="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100"
+              class="group h-12 px-6 border-2 border-gray-300 dark:border-gray-700 rounded-full transition duration-300 hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100"
             >
               <div class="relative flex items-center space-x-4 justify-center">
                 <svg
@@ -61,7 +61,7 @@
               </div>
             </button>
             <button
-              class="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100"
+              class="group h-12 px-6 border-2 border-gray-300 dark:border-gray-700 rounded-full transition duration-300 hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100"
             >
               <div class="relative flex items-center space-x-4 justify-center">
                 <img
