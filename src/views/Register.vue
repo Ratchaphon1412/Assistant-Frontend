2ccf3eeef454182145ca167c1d023dbd2520aac6
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-02-20 18:29:02 +0700

    create register views

---
+++ src/views/Register.vue
@@ -0,0 +1,14 @@
+<template>
+  <NavbarFooter> <RegisterCard /> </NavbarFooter>
+</template>
+<script>
+import RegisterCard from "@/components/RegisterCard.vue";
+import NavbarFooter from "@/layouts/NavbarFooter.vue";
+export default {
+  name: "Register",
+  components: {
+    RegisterCard,
+    NavbarFooter,
+  },
+};
+</script>
