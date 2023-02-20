69f08f208a425e3208cc7b3b01f0c287f73506f5
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-02-20 14:23:37 +0700

    install axios

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -136,7 +136,7 @@
     <!--expand -->
     <div
       id="mega-menu-full-dropdown"
-      class="mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600"
+      class="mt-1 mb-8 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600"
     >
       <div
         class="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6"
