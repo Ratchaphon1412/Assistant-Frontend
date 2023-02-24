17418658d0648085d837ccd3452302fb79974cd3
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-02-24 15:08:22 +0700

    css nano on production and use jit engine to simplify tailwind when build for production to small size and add theme sceen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  darkMode:'media',
   content: [
      "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {},
   },
   plugins: [
